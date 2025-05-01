aaea66128d5974bdfc8af5bf7ab7d406ce7898c2
Chukwu Rophi <chukwurophi@gmail.com>
2025-05-01 17:16:37 +0100

    feat: added the editEmployee adn editEmployeeAttribute controllers

--- src/controllers/editEmployee.ts
+++ src/controllers/editEmployee.ts
@@ -12,6 +12,11 @@ export function editEmployee(req: Request, res: Response){
 
         delete employeeData.id;
 
+        if(!employeeData.name){ res.status(401).send({ message: 'Employee name must be provided' }); return; }
+        if(!employeeData.gender){ res.status(401).send({ message: 'Employee gender must be provided' }); return; }
+        if(!employeeData.phone){ res.status(401).send({ message: 'Employee phone must be provided' }); return; }
+        if(!employeeData.salary){ res.status(401).send({ message: 'Employee salary must be provided' }); return; }
+
         const userIndex = employees.findIndex(employee => employee.id === id);
 
         if(userIndex===-1){
